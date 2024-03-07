22b1a21084fdc01a7e0032d3c2af0b7ac2458119
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-03-07 22:28:42 +0100

    Create dataCheck.php

---
+++ dataCheck.php
@@ -0,0 +1,28 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nombreJ;
+    $colorJ;
+    $colores = [];
+    $errors = [];
+    for ($i=1; $i < 4; $i++) { 
+        if (isset($_POST['nombre'+$i]) && isset($_POST['color'+$i])) {
+            $nombreJ = trim(htmlspecialchars($_POST['nombre'+$i]));
+            $colorJ = trim(htmlspecialchars($_POST['color'+$i]));
+            if(!empty($nombreJ) && !empty($colorJ)) {
+                if(!in_array($colorJ,$colores)){
+                    array_push($colores,$colorJ);
+                }else{
+                    #Color repetido
+                    array_push($errors,"2 jugadores no pueden elegir el mismo color.");
+                }
+            }else {
+                #Existe pero está vacio
+                array_push($errors,"Rellena todos los datos");
+            }
+        }
+        #No existe
+    }
+    if (empty($errors)) {
+        header("Location: "); #Envia al tablero
+    }
+}
