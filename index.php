c1a17737a5419e1cce90080bb9d1c131182d6c30
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-03-07 21:47:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Juego Oca</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <form action="index.php">
+    <form action="<?= $_SERVER['PHP_SELF']?>">
         <!--<p>N&uacute;mero de jugadores: 
             <input type="number" id= "jugadores" min="2" max="4"/>
         </p>-->
-        <div id= "jugadores">
+        <div id="jugadores">
             <h2>Elige jugadores para comenzar</h2>
         </div>
         <input type="submit" value="Iniciar partida!"/>
     </form>
     <script src="script.js"></script>
-    <div id = "botones">
-        <button id="sumarJ" onclick = addPlayer() display="block">Añadir Jugador</button>
-        <button id="restarJ" onclick = removePlayer() display="none">Quitar Jugador</button>
+    <div id="botones">
+        <button id="sumarJ" onclick="addPlayer()">Añadir Jugador</button>
+        <button id="restarJ" onclick="removePlayer()" style="display: none;">Quitar Jugador</button>
     </div>
-    
 </body>
 </html>
